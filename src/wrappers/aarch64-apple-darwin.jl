1c7ea49c936e3c9a32ceb4466b06a245ee4c9e7f
jlbuild <juliabuildbot@gmail.com>
2025-01-31 16:01:38 +0000

    Bzip2_jll build 1.0.9+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libbzip2,
-        "lib/libbz2.1.0.8.dylib",
+        "lib/libbz2.1.0.9.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
